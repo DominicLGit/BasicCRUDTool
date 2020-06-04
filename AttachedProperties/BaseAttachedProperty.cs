39e90df4329fb325e2b7e69a1b29037a1ce36ae9
Dominic <Dominic@CONTROLLER>
2020-06-04 16:03:01 +0100

    Added BaseAttachedProperty

---
+++ AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+
+namespace BasicCRUDTool
+{
+    /// <summary>
+    /// replaces WPF attached property
+    /// </summary>
+    /// <typeparam name="Parent">Type of attached property</typeparam>
+    /// <typeparam name="Property">parent class to be the attached property</typeparam>
+    public abstract class BaseAttachedProperty<Parent, Property>
+        where Parent : BaseAttachedProperty<Parent, Property>, new()
+    {
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+        /// <summary>
+        /// Singleton instance of parent class
+        /// </summary>
+        public static Parent Instance { get; private set; } = new Parent();
+
+        /// <summary>
+        /// The attached property for this class
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.RegisterAttached("Value", typeof(Property), typeof(BaseAttachedProperty<Parent, Property>), new PropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+        
+        /// <summary>
+        /// Callback event when <see cref="ValueProperty"/> is changed
+        /// </summary>
+        /// <param name="d">The ui element that had its property changed</param>
+        /// <param name="e">The arguments for the event</param>
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // Call the parent function
+            Instance.ValueChanged(d, e);
+            // Call event listeners
+            Instance.ValueChanged(d, e);
+        }
+
+        /// <summary>
+        /// Gets the attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <returns></returns>
+        public static Property GetValue(DependencyObject d) => (Property)d.GetValue(ValueProperty);
+
+        /// <summary>
+        /// Sets attached property
+        /// </summary>
+        /// <param name="d">The element to get the property from</param>
+        /// <param name="value">The value to set the property to</param>
+        public static void SetValue(DependencyObject d, Property value) => d.SetValue(ValueProperty, value);
+
+        /// <summary>
+        /// Method that is called when any attached property of this type is changed
+        /// </summary>
+        /// <param name="sender">The element to this property was changed for</param>
+        /// <param name="e">The arguments for this event</param>
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) { }
+
+    }
+}
